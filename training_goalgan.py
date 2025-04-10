4b75020679f96630e1e1685f55188b49ce95379c
Kathryn L. Wantlin <kw2960@node011.ionic.cs.princeton.edu>
2025-04-10 12:48:35 -0400

    progress checkpoint

--- training_goalgan.py
+++ training_goalgan.py
@@ -8,7 +8,7 @@ from brax.io import model
 from pyinstrument import Profiler
 
 
-from src.train import train
+from src.goalgan_train import train
 from utils import MetricsRecorder, get_env_config, create_env, create_eval_env, create_parser, render
 
 
