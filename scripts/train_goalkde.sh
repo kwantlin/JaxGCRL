8bcb0e76cf9c261150e60ce1ce4c2073e8b79695
kwantlin <kwantlin@princeton.edu>
2025-04-14 05:05:50 -0400

    goalkde bash bug fix

--- scripts/train_goalkde.sh
+++ scripts/train_goalkde.sh
@@ -12,7 +12,7 @@ conda activate jaxgcrl
 env=ant
 
 for seed in 1 ; do
-  XLA_PYTHON_CLIENT_MEM_FRACTION=.95 MUJOCO_GL=egl CUDA_VISIBLE_DEVICES=0 python training_goal_kde.py \
+  XLA_PYTHON_CLIENT_MEM_FRACTION=.95 MUJOCO_GL=egl CUDA_VISIBLE_DEVICES=0 python training_goalkde.py \
     --project_name test --group_name first_run --exp_name ${env}-goalkde --num_evals 50 \
     --seed ${seed} --num_timesteps 10000000 --batch_size 256 --num_envs 512 \
     --discounting 0.99 --action_repeat 1 --env_name ${env} \
