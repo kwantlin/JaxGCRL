fcd768c638cd29b5b2247e6dd8a73615c422c61f
kwantlin <kwantlin@kamaji.cs.princeton.edu>
2025-04-14 02:42:32 -0400

    bc training updates (haven't run yet, might be buggy)

--- scripts/train.sh
+++ scripts/train.sh
@@ -8,16 +8,71 @@
 eval "$(conda shell.bash hook)"
 conda activate jaxgcrl
 
-env=ant
+
+env=simple_u_maze
 
 for seed in 1 ; do
   XLA_PYTHON_CLIENT_MEM_FRACTION=.95 MUJOCO_GL=egl CUDA_VISIBLE_DEVICES=0 python training.py \
-    --project_name test --group_name first_run --exp_name ${env}-main-meanfield-encoded --num_evals 50 \
-    --seed ${seed} --num_timesteps 10000000 --batch_size 256 --num_envs 512 \
+    --project_name test --group_name first_run --exp_name ${env}-main --num_evals 50 \
+    --seed ${seed} --num_timesteps 10000000 --batch_size 1024 --num_envs 256 \
     --discounting 0.99 --action_repeat 1 --env_name ${env} \
     --episode_length 1025 --unroll_length 62  --n_hidden 8 --min_replay_size 1000 --max_replay_size 10000 \
     --contrastive_loss_fn infonce_backward --energy_fn l2 \
-    --train_step_multiplier 1 --log_wandb --var_post meanfield_encoded
+    --train_step_multiplier 1 --log_wandb --var_post standard
+  done
+
+for seed in 1 ; do
+  XLA_PYTHON_CLIENT_MEM_FRACTION=.95 MUJOCO_GL=egl CUDA_VISIBLE_DEVICES=0 python training.py \
+    --project_name test --group_name first_run --exp_name ${env}-main-meanfield --num_evals 50 \
+    --seed ${seed} --num_timesteps 10000000 --batch_size 1024 --num_envs 256 \
+    --discounting 0.99 --action_repeat 1 --env_name ${env} \
+    --episode_length 1025 --unroll_length 62  --n_hidden 8 --min_replay_size 1000 --max_replay_size 10000 \
+    --contrastive_loss_fn infonce_backward --energy_fn l2 \
+    --train_step_multiplier 1 --log_wandb --var_post meanfield
+  done
+
+env=reacher
+
+for seed in 1 ; do
+  XLA_PYTHON_CLIENT_MEM_FRACTION=.95 MUJOCO_GL=egl CUDA_VISIBLE_DEVICES=0 python training.py \
+    --project_name test --group_name first_run --exp_name ${env}-main --num_evals 50 \
+    --seed ${seed} --num_timesteps 10000000 --batch_size 1024 --num_envs 256 \
+    --discounting 0.99 --action_repeat 1 --env_name ${env} \
+    --episode_length 1025 --unroll_length 62  --n_hidden 8 --min_replay_size 1000 --max_replay_size 10000 \
+    --contrastive_loss_fn infonce_backward --energy_fn l2 \
+    --train_step_multiplier 1 --log_wandb --var_post standard
+  done
+
+for seed in 1 ; do
+  XLA_PYTHON_CLIENT_MEM_FRACTION=.95 MUJOCO_GL=egl CUDA_VISIBLE_DEVICES=0 python training.py \
+    --project_name test --group_name first_run --exp_name ${env}-main-meanfield --num_evals 50 \
+    --seed ${seed} --num_timesteps 10000000 --batch_size 1024 --num_envs 256 \
+    --discounting 0.99 --action_repeat 1 --env_name ${env} \
+    --episode_length 1025 --unroll_length 62  --n_hidden 8 --min_replay_size 1000 --max_replay_size 10000 \
+    --contrastive_loss_fn infonce_backward --energy_fn l2 \
+    --train_step_multiplier 1 --log_wandb --var_post meanfield
+  done
+
+env=pusher_easy
+
+for seed in 1 ; do
+  XLA_PYTHON_CLIENT_MEM_FRACTION=.95 MUJOCO_GL=egl CUDA_VISIBLE_DEVICES=0 python training.py \
+    --project_name test --group_name first_run --exp_name ${env}-main --num_evals 50 \
+    --seed ${seed} --num_timesteps 30000000 --batch_size 1024 --num_envs 256 \
+    --discounting 0.99 --action_repeat 1 --env_name ${env} \
+    --episode_length 1025 --unroll_length 62  --n_hidden 8 --min_replay_size 1000 --max_replay_size 10000 \
+    --contrastive_loss_fn infonce_backward --energy_fn l2 \
+    --train_step_multiplier 1 --log_wandb --var_post standard
+  done
+
+for seed in 1 ; do
+  XLA_PYTHON_CLIENT_MEM_FRACTION=.95 MUJOCO_GL=egl CUDA_VISIBLE_DEVICES=0 python training.py \
+    --project_name test --group_name first_run --exp_name ${env}-main-meanfield --num_evals 50 \
+    --seed ${seed} --num_timesteps 30000000 --batch_size 1024 --num_envs 256 \
+    --discounting 0.99 --action_repeat 1 --env_name ${env} \
+    --episode_length 1025 --unroll_length 62  --n_hidden 8 --min_replay_size 1000 --max_replay_size 10000 \
+    --contrastive_loss_fn infonce_backward --energy_fn l2 \
+    --train_step_multiplier 1 --log_wandb --var_post meanfield
   done
 
 echo "All runs have finished."
